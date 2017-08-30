2cc305b6cf29c35246744aaaa0f6e597b6e698a9
Shane Sievers <shane@mediadezine.com>
2017-08-29 22:15:15 -0400

    A few class changea and lots of nav chagnes

---
+++ RARGenerator/Utilities/Extensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RARGenerator.Utilities
+{
+    public static class Extensions
+    {
+        public static string GetNavigationClasses(bool isValid, bool wasStarted, string currentAction, string actionToTest)
+        {
+            string classes = "";
+
+            if (isValid)
+            {
+                classes = "complete";
+            }
+
+            if (wasStarted && !isValid)
+            {
+                classes = "incomplete";
+            }
+
+            if (currentAction.ToLower() == actionToTest.ToLower())
+            {
+                classes += " active";
+            }
+
+            return classes;
+        }
+    }
+}
